e9d8447093b31909409c46d46fb01e9a6f52e40f
KaranBhela <bkaranpreet@gmail.com>
2021-10-16 21:14:11 -0400

    This is my second Maven

---
+++ src/main/java/git/practiceMaven2/git/practiceMaven2/App.java
@@ -0,0 +1,13 @@
+package git.practiceMaven2.git.practiceMaven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/git/practiceMaven2/git/practiceMaven2/AppTest.java
@@ -0,0 +1,20 @@
+package git.practiceMaven2.git.practiceMaven2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
